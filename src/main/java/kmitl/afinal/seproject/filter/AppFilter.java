c4d9249c2eccf255da54c66c1d6965b9ddacede8
belizwp <belizwp@live.com>
2017-12-14 12:27:15 +0700

    update filter

--- src/main/java/kmitl/afinal/seproject/filter/AppFilter.java
+++ src/main/java/kmitl/afinal/seproject/filter/AppFilter.java
@@ -10,10 +10,11 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
 
-@WebFilter(filterName = "AppFilter", urlPatterns = "/*")
+@WebFilter(filterName = "AppFilter",
+        urlPatterns = {"/index.jsp", "/Profile.jsp", "/Search.jsp", "/sheet.jsp", "/subject.jsp", "/upload.jsp"},
+        servletNames = {"InfoCollectorServlet", "SearchServlet", "SearchSubjectServlet",
+                "UploadServlet", "viewSheetAllServlet", "ViewSheetMeServlet"})
 public class AppFilter implements Filter {
-
-    private ArrayList<String> urlList;
 
     public void destroy() {
     }
@@ -21,35 +22,23 @@ public class AppFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        String url = request.getServletPath();
-        boolean allowedRequest = false;
 
-        if (urlList.contains(url)) {
-            allowedRequest = true;
-        }
-
-        if (!allowedRequest) {
-            try {
-                HttpSession session = request.getSession(false);
-                User user = (User) session.getAttribute("user");
-                if (user == null) {
-                    throw new Exception("need logged in");
-                }
-            } catch (Exception e) {
-                response.sendRedirect("/Login.jsp");
-                return;
+        try {
+            HttpSession session = request.getSession(false);
+            User user = (User) session.getAttribute("user");
+            if (user == null) {
+                throw new Exception("need logged in");
             }
+        } catch (Exception e) {
+            response.sendRedirect("/Login.jsp");
+            return;
         }
 
         chain.doFilter(servletRequest, servletResponse);
     }
 
     public void init(FilterConfig config) throws ServletException {
-        urlList = new ArrayList<String>();
-        urlList.add("/Login.jsp");
-        urlList.add("/login");
-        urlList.add("/Register.jsp");
-        urlList.add("/register");
+
     }
 
 }
